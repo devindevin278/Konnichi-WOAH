1303b51726eba0381f38b64aec82bcc34f0f0f71
michaelbaptis <michaelbaptis@gmail.com>
2023-05-10 06:20:08 +0700

    update landing page

--- resources/views/student/homeStudent.blade.php
+++ resources/views/student/homeStudent.blade.php
@@ -1,8 +1,221 @@
 @extends('layouts.mainStudent')
 
 @section('content')
+<div>
+    {{-- <link href="style4.css" rel="stylesheet" /> --}}
 
-
-<h1>cokkk</h1>
+    <div class="landing-page-container">
+      <div class="landing-page-landing-page">
+        <div class="landing-page-header">
+            <button class="landing-page-button">
+            <img
+              src="img/home/kotakbutton2062-j0va-200h.png"
+              alt="kotakbutton2062"
+              class="landing-page-kotakbutton"
+            />
+            <span class="landing-page-text04"><span>Learn More</span></span>
+          </button>
+          <span class="landing-page-text06">
+            <span>
+              Are you looking for an easy and fun way to learn Japanese? Look
+              no further than Konnichiwoah! We offer a wide range of exciting
+              features to help you master the Japanese language with ease and
+              effectiveness.
+            </span>
+          </span>
+          <span class="landing-page-text08">
+            <span>
+              <span>Welcome to Konnichi-WOAH!</span>
+              <br />
+              <span>A captivating Japanese language learning platform!</span>
+            </span>
+          </span>
+          <img
+            src="img/home/kucing2062-wjq-500w.png"
+            alt="kucing2062"
+            class="landing-page-kucing"
+          />
+        </div>
+        <div class="landing-page-desc">
+          <span class="landing-page-text13">
+            <span>
+              <span>
+                At Konnichi-WOAH!, we are passionate about making Japanese
+                language learning accessible to everyone. Our platform was
+                founded by a team of experienced Japanese language teachers
+                who believe that learning a language should be an enjoyable
+                and rewarding experience.
+              </span>
+              <br />
+              <span></span>
+              <br />
+              <span>
+                Our mission is to provide a user-friendly platform that caters
+                to different learning styles, levels, and preferences. We are
+                committed to helping our users achieve their language learning
+                goals with ease and effectiveness.
+              </span>
+            </span>
+          </span>
+          <span class="landing-page-text20">
+            <span>
+              We are the team behind Konnichi-WOAH! - a leading platform for
+              learning the Japanese language.
+            </span>
+          </span>
+          <span class="landing-page-text22"><span>WHO WE ARE</span></span>
+        </div>
+        <div class="landing-page-isi">
+          <div class="landing-page-article">
+            <span class="landing-page-text24">
+              <span>
+                Konnichiwoah! also offers captivating articles on Japanese
+                culture and history, enabling learners to gain insights and
+                enrich their knowledge about Japan.
+              </span>
+            </span>
+            <span class="landing-page-text26"><span>ARTICLE</span></span>
+            <img
+              src="img/home/imgarticle2062-vw7f.svg"
+              alt="imgarticle2062"
+              class="landing-page-imgarticle"
+            />
+          </div>
+          <div class="landing-page-teacher">
+            <span class="landing-page-text28">
+              <span>
+                For those serious about improving their Japanese language
+                skills, Konichiwoah! provides a list of qualified teachers who
+                can assist with Japanese grammar and offer additional lessons
+              </span>
+            </span>
+            <span class="landing-page-text30"><span>TEACHER</span></span>
+            <div class="landing-page-imgteacher">
+              <img
+                src="img/home/vector2062-9n1v.svg"
+                alt="Vector2062"
+                class="landing-page-vector14"
+              />
+              <img
+                src="img/home/vector2062-5pap.svg"
+                alt="Vector2062"
+                class="landing-page-vector15"
+              />
+            </div>
+          </div>
+          <div class="landing-page-games">
+            <span class="landing-page-text32">
+              <span>
+                KoNnichiwoah! offers Hiragana and Katakana Cards, as well as
+                language games, to make learning Japanese characters and
+                language skills easy and enjoyable.
+              </span>
+            </span>
+            <span class="landing-page-text34"><span>GAMES</span></span>
+            <img
+              src="img/home/imggames2062-wls.svg"
+              alt="imggames2062"
+              class="landing-page-imggames"
+            />
+          </div>
+        </div>
+        <div class="landing-page-aboutus">
+          <div class="landing-page-devin">
+            <img
+              src="img/home/imgdevin7776-5yki-300w.png"
+              alt="imgdevin7776"
+              class="landing-page-imgdevin"
+            />
+            <img
+              src="img/home/kotakdevin7776-zhktb-300h.png"
+              alt="kotakdevin7776"
+              class="landing-page-kotakdevin"
+            />
+            <span class="landing-page-text36">
+              <span>
+                Konichiwoah! also offers captivating articles on Japanese
+                culture and history, enabling learners to gain insights and
+                enrich their knowledge about Japan. Konichiwoah! also offers
+                captivating articles on Japanese culture and history, enabling
+                learners to gain insights and enrich their knowledge about
+                Japan.
+              </span>
+            </span>
+          </div>
+          <div class="landing-page-hasna">
+            <img
+              src="img/home/imghasna7776-4qce-300w.png"
+              alt="imghasna7776"
+              class="landing-page-imghasna"
+            />
+            <img
+              src="img/home/kotakhasna7776-yzq7-300h.png"
+              alt="kotakhasna7776"
+              class="landing-page-kotakhasna"
+            />
+            <span class="landing-page-text38">
+              <span>
+                Konichiwoah! also offers captivating articles on Japanese
+                culture and history, enabling learners to gain insights and
+                enrich their knowledge about Japan. Konichiwoah! also offers
+                captivating articles on Japanese culture and history, enabling
+                learners to gain insights and enrich their knowledge about
+                Japan.
+              </span>
+            </span>
+          </div>
+          <div class="landing-page-hansen">
+            <img
+              src="img/home/imghansen7776-lrj-300w.png"
+              alt="imghansen7776"
+              class="landing-page-imghansen"
+            />
+            <img
+              src="img/home/kotakhansen7776-ymdg-300h.png"
+              alt="kotakhansen7776"
+              class="landing-page-kotakhansen"
+            />
+            <span class="landing-page-text40">
+              <span>
+                Konichiwoah! also offers captivating articles on Japanese
+                culture and history, enabling learners to gain insights and
+                enrich their knowledge about Japan. Konichiwoah! also offers
+                captivating articles on Japanese culture and history, enabling
+                learners to gain insights and enrich their knowledge about
+                Japan.
+              </span>
+            </span>
+          </div>
+          <div class="landing-page-mike">
+            <img
+              src="img/home/imgmike7776-hpe-300w.png"
+              alt="imgmike7776"
+              class="landing-page-imgmike"
+            />
+            <img
+              src="img/home/kotakmike7776-n88-300h.png"
+              alt="kotakmike7776"
+              class="landing-page-kotakmike"
+            />
+            <span class="landing-page-text42">
+              <span>
+                Konichiwoah! also offers captivating articles on Japanese
+                culture and history, enabling learners to gain insights and
+                enrich their knowledge about Japan. Konichiwoah! also offers
+                captivating articles on Japanese culture and history, enabling
+                learners to gain insights and enrich their knowledge about
+                Japan.
+              </span>
+            </span>
+          </div>
+          <span class="landing-page-text44"><span>ABOUT US</span></span>
+          <img
+            src="img/home/batas7776-25ue.svg"
+            alt="batas7776"
+            class="landing-page-batas"
+          />
+        </div>
+      </div>
+    </div>
 
 @endsection
