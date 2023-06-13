7e12c491915aab5ed2e79d6d222a249717a7b74f
Hansen <hh7662891@gmail.com>
2023-06-13 13:46:19 +0700

    notebook+gif

---
+++ resources/views/student/notebook1.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="node_modules/@popperjs/core/dist/umd/popper.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="{{url('book.css')}}">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins" >
+</head>
+<body>
+    <div class="note-page">
+        <a href="">
+            <svg class="note-back-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+              </svg>
+        </a>
+
+        <div class="note-header">
+            <img src="/Assets/notecat.png" class="note-img" style="margin-top: -1vw">
+
+            <div class="note-title">
+                <h1>
+                    Level 1 Vocabularies Notebook
+                </h1>
+                <h2>*Judul bab nya*</h2>
+            </div>
+                <img src="Assets/breadcat.png" class="note-img" alt="">
+
+        </div>
+
+            <div class="note-list">
+                <div class="note-cont">
+                    <h3>おはよう</h3>
+                    <h4>Ohayou</h4>
+                    <h4>Good morning </h4>
+                </div>
+
+                <div class="note-cont">
+                    <h3>おじゃまします </h3>
+                    <h4>Ojama shimasu </h4>
+                    <h4>Excuse me</h4>
+                </div>
+                <div class="note-cont">
+                    <h3>おつかれさまです </h3>
+                    <h4>Otsukare-sama desu</h4>
+                    <h4>Thank you for your hard work</h4>
+                </div>
+                <div class="note-cont">
+                    <h3>おはよう</h3>
+                    <h4>Ohayou</h4>
+                    <h4>Good morning </h4>
+                </div>
+                <div class="note-cont">
+                    <h3>おはよう</h3>
+                    <h4>Ohayou</h4>
+                    <h4>Good morning </h4>
+                </div>
+                <div class="note-cont">
+                    <h3>おはよう</h3>
+                    <h4>Ohayou</h4>
+                    <h4>Good morning </h4>
+                </div>
+                <div class="note-cont">
+                    <h3>おはよう</h3>
+                    <h4>Ohayou</h4>
+                    <h4>Good morning </h4>
+                </div>
+                <div class="note-cont">
+                    <h3>おはよう</h3>
+                    <h4>Ohayou</h4>
+                    <h4>Good morning </h4>
+                </div>
+                <div class="note-cont">
+                    <h3>おはよう</h3>
+                    <h4>Ohayou</h4>
+                    <h4>Good morning </h4>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</body>
+</html>
