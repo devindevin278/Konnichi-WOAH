676d19d2173e5c9136acb50381172c7ca5a20e60
Hasna Salsabilla <hasnas291@gmail.com>
2023-06-26 16:02:11 +0700

    commitdeh

--- resources/views/teacher/profileTeacher.blade.php
+++ resources/views/teacher/profileTeacher.blade.php
@@ -29,10 +29,10 @@
 
                     @if ($user->photo)
                         <div class="d-flex justify-content-around mb-3">
-                            <img class="rounded-circle mt-5" style="
+                            <img class="rounded-circle mt-5 shadow" style="
                             background-color: solid #FF8BA7;
-                            width: 150px;
-                            height: 150px;
+                            width: 170px;
+                            height: 170px;
                             border-radius: 50%;
                             overflow:hidden; object-fit: cover;
                             " src="{{ asset('storage/'. $user->photo) }}">
@@ -40,9 +40,9 @@
 
                     @else
                         <div class="d-flex justify-content-around mb-3">
-                            <img class="rounded-circle mt-5" style="
-                            width: 150px;
-                            height: 150px;
+                            <img class="rounded-circle mt-5 shadow" style="
+                            width: 170px;
+                            height: 170px;
                             overflow:hidden;
                             " src="{{ asset('img/profilespic.png') }}">
                         </div>
@@ -50,25 +50,22 @@
 
 
 
-                    <div class="d-flex justify-content-around mt-3">
-                        <form action="/logout" method="post" style="
-                        text-align: center;
-                        width: 80px;
-                        background: #FF8BA7;
-                        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
-                        border-radius: 10px;">
-                            @csrf
-                            <button type="submit" class="dropdown-item" style="font-weight:500;"> Logout</button>
-                        </form>
-                    </div>
+                    <div class="d-flex col justify-content-center">
+                        <div class="d-flex justify-content-around mt-3" >
 
-                    <div class="d-flex justify-content-around mt-3" >
+                            <form action="/profileTeacher/{{ $user->id }}" method="post" class="d-inline">
+                                @method('delete')
+                                @csrf
+                                <button class="logoutedit border-0 bg-white" onclick="return confirm('Are you sure u wanna delete?')"><svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 7H20" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 10L7.70141 19.3578C7.87432 20.3088 8.70258 21 9.66915 21H14.3308C15.2974 21 16.1257 20.3087 16.2986 19.3578L18 10" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
+                            </form>
+                        </div>
 
-                        <form action="/profileTeacher/{{ $user->id }}" method="post" class="d-inline">
-                            @method('delete')
-                            @csrf
-                            <button class="badge bg-danger border-0" onclick="return confirm('Are you sure u wanna delete?')">Delete Account</button>
-                        </form>
+                        <div class="d-flex justify-content-around mt-3" class="logoutedit">
+                            <form action="/logout" method="post" >
+                                @csrf
+                                <button type="submit" class="logoutedit border-0 bg-white" style="font-weight:500;"> <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 12L13 12" stroke="#FF8BA7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9" stroke="#FF8BA7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19" stroke="#FF8BA7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
