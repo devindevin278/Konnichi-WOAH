eba6fb3fcaf7cd6bc9a9127ca2f530f7298777b7
devindevin278 <devindevin278@gmail.com>
2023-07-03 09:01:58 +0700

    teacherSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -55,13 +55,62 @@ class UserSeeder extends Seeder
             'name' => 'Sensei',
             'userIsTeacher' => 1,
             'email' => 'teacher@gmail.com',
-            'password' => Hash::make('12345')
+            'password' => Hash::make('12345'),
+            'phoneNumber' => '08123456778',
+            'gender' => 0,
+            'DOB' => '2023-07-03',
+            'address' => 'Jalan Achmad Yani',
+            'province' => 'West Kalimantan',
+            'city' => 'Pontianak',
+            'price' => 100000,
+            'photo' => 'profile-images/teacher1.jpg',
+            'descteacher' => 'Dapatkan pengalaman belajar bahasa Jepang terbaik sekarang juga!'
         ]);
+
         User::create([
             'name' => 'Master',
             'userIsTeacher' => 1,
             'email' => 'teacher2@gmail.com',
-            'password' => Hash::make('12345')
+            'password' => Hash::make('12345'),
+            'phoneNumber' => '08123456778',
+            'gender' => 1,
+            'DOB' => '2023-07-03',
+            'address' => 'Jalan Achmad Yani',
+            'province' => 'Bali',
+            'city' => 'Denpasar',
+            'price' => 200000,
+            'photo' => 'profile-images/teacher2.jpg',
+            'descteacher' => 'Konnichiwa! Daftar sekarang!'
+        ]);
+        User::create([
+            'name' => 'Professor',
+            'userIsTeacher' => 1,
+            'email' => 'teacher3@gmail.com',
+            'password' => Hash::make('12345'),
+            'phoneNumber' => '08123456778',
+            'gender' => 0,
+            'DOB' => '2023-07-03',
+            'address' => 'Jalan Achmad Yani',
+            'province' => 'West Sumatra',
+            'city' => 'Padang',
+            'price' => 300000,
+            'photo' => 'profile-images/teacher3.jpg',
+            'descteacher' => 'Pengajar berkualitas tinggi'
+        ]);
+        User::create([
+            'name' => 'Master Sensei Turbo',
+            'userIsTeacher' => 1,
+            'email' => 'teacher4@gmail.com',
+            'password' => Hash::make('12345'),
+            'phoneNumber' => '08123456778',
+            'gender' => 1,
+            'DOB' => '2023-07-03',
+            'address' => 'Jalan Achmad Yani',
+            'province' => 'Jambi',
+            'city' => 'Jambi',
+            'price' => 400000,
+            'photo' => 'profile-images/teacher4.jpg',
+            'descteacher' => 'Konnichiwa! Daftar sekarang!'
         ]);
     }
 }
